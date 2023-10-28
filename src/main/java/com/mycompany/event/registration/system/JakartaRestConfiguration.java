464bbeff3ef0ca0d1ecb92eea21f38c3d94f6729
Alperen Aga <alperencode@gmail.com>
2023-10-28 14:48:08 +0300

    Initialize JSP structure

---
+++ src/main/java/com/mycompany/event/registration/system/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.event.registration.system;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
